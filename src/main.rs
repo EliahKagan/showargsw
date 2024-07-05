1e2ccacd4f5bae691842509b97c45fbf22510e5e
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-05 01:58:54 -0400

    Revert "Try SetProcessDpiAwarenessContext"
    Because it didn't provide larger or otherwise better mesage box
    text than the older and more widely compatible function.
    
    This reverts commit 56711cb0f75fe599f7bbf262e55e778df61afd6b.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use windows::Win32::UI::{
-    HiDpi::{DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2, SetProcessDpiAwarenessContext},
-    WindowsAndMessaging::{MB_OK, MessageBoxW},
+use windows::Win32::UI::WindowsAndMessaging::{
+    MB_OK,
+    MessageBoxW,
+    SetProcessDPIAware,
 };
 use windows::core::w;
 
 fn main() {
     unsafe {
-        if let Err(e) = SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2) {
-            panic!("Couldn't respect display scaling: {:?}", e);
+        if !SetProcessDPIAware().as_bool() {
+            panic!("Couldn't respect display scaling");
         }
         MessageBoxW(
             None,
