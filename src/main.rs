64bd059b89275db91012648a7c19c2e3c9c57197
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-05 02:43:13 -0400

    Make it report the command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-use windows::Win32::UI::WindowsAndMessaging::{
-    MB_OK,
-    MessageBoxW,
-    SetProcessDPIAware,
+use std::{env, iter};
+
+use windows::{
+    core::{w, PCWSTR},
+    Win32::UI::WindowsAndMessaging::{MessageBoxW, SetProcessDPIAware, MB_ICONINFORMATION, MB_OK},
 };
-use windows::core::w;
 
 fn main() {
+    let lines: Vec<String> = env::args()
+        .enumerate()
+        .map(|(i, arg)| format!("{i}: [{arg}]"))
+        .collect();
+    let message: Vec<u16> = lines
+        .join("\n")
+        .encode_utf16()
+        .chain(iter::once(0))
+        .collect();
+
     unsafe {
         if !SetProcessDPIAware().as_bool() {
-            panic!("Couldn't respect display scaling");
+            eprintln!("Couldn't respect display scaling!");
         }
         MessageBoxW(
             None,
-            w!("Hello, world!"),
-            w!("Greetings"),
-            MB_OK,
+            PCWSTR(message.as_ptr()),
+            w!("Command-line arguments"),
+            MB_OK | MB_ICONINFORMATION,
         );
     }
 }
