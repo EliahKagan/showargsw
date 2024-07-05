b15cc91344f9b934af76fd722d3ff7992e7a35bd
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-05 01:49:20 -0400

    Try SetProcessDPIAware
    Because the message box window is blurry when display scaling is
    enabled.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use windows::Win32::UI::WindowsAndMessaging::{
     MB_OK,
     MessageBoxW,
+    SetProcessDPIAware,
 };
 use windows::core::w;
 
 fn main() {
     unsafe {
+        if !SetProcessDPIAware().as_bool() {
+            panic!("Couldn't respect display scaling");
+        }
         MessageBoxW(
             None,
             w!("Hello, world!"),
