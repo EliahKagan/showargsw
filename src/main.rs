1ab9fe6b14a910ce994d007050d4efbe1c6fb9ea
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-05 18:26:09 -0400

    Report more errors and have less in unsafe blocks

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,47 @@
-use std::{env, iter};
+//! Displays numbered command-line arguments in a Windows message box.
 
 use windows::{
-    core::{w, PCWSTR},
-    Win32::UI::WindowsAndMessaging::{MessageBoxW, SetProcessDPIAware, MB_ICONINFORMATION, MB_OK},
+    core::{w, Error, PCWSTR},
+    Win32::UI::WindowsAndMessaging::{
+        MessageBoxW, SetProcessDPIAware, MB_ICONINFORMATION, MB_OK, MESSAGEBOX_RESULT,
+    },
 };
 
-fn main() {
-    let lines: Vec<String> = env::args()
+/// The text may still be too small, but this at least keeps it from being blurry.
+fn attempt_dpi_awareness() {
+    let dpi_aware = unsafe { SetProcessDPIAware() };
+
+    if !dpi_aware.as_bool() {
+        eprintln!("Couldn't respect display scaling!");
+    }
+}
+
+fn main() -> Result<(), Error> {
+    attempt_dpi_awareness();
+
+    let lines: Vec<String> = std::env::args()
         .enumerate()
         .map(|(i, arg)| format!("{i}: [{arg}]"))
         .collect();
+
     let message: Vec<u16> = lines
         .join("\n")
         .encode_utf16()
-        .chain(iter::once(0))
+        .chain(std::iter::once(0))
         .collect();
 
-    unsafe {
-        if !SetProcessDPIAware().as_bool() {
-            eprintln!("Couldn't respect display scaling!");
-        }
+    let mb_result = unsafe {
         MessageBoxW(
             None,
             PCWSTR(message.as_ptr()),
             w!("Command-line arguments"),
             MB_OK | MB_ICONINFORMATION,
-        );
+        )
+    };
+
+    if mb_result == MESSAGEBOX_RESULT(0) {
+        Err(Error::from_win32())
+    } else {
+        Ok(())
     }
 }
