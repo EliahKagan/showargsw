03eee00a74cffd47a17fc1c1e72bcf2bebbae0b7
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-06 01:41:15 -0400

    Restore MessageBoxW way, since TaskDialog was no better

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,47 @@
-//! Displays numbered command-line arguments in a Windows dialog box.
+//! Displays numbered command-line arguments in a Windows message box.
 
 use windows::{
     core::{w, Error, PCWSTR},
-    Win32::System::Com::{CoInitializeEx, COINIT_APARTMENTTHREADED},
-    Win32::UI::Controls::{
-        InitCommonControlsEx, TaskDialog, ICC_STANDARD_CLASSES, INITCOMMONCONTROLSEX,
-        TDCBF_CLOSE_BUTTON, TD_INFORMATION_ICON,
+    Win32::UI::WindowsAndMessaging::{
+        MessageBoxW, SetProcessDPIAware, MB_ICONINFORMATION, MB_OK, MESSAGEBOX_RESULT,
     },
 };
 
-/// Allow COM functionality to be used on this thread.
-/// FIXME: This needs a matching CoUninitialize() call!
-fn initialize_com() -> Result<(), Error> {
-    let result = unsafe { CoInitializeEx(None, COINIT_APARTMENTTHREADED) };
-    if result.is_err() {
-        Err(Error::from_hresult(result))
-    } else {
-        Ok(())
+/// The text may still be too small, but this at least keeps it from being blurry.
+fn attempt_dpi_awareness() {
+    let dpi_aware = unsafe { SetProcessDPIAware() };
+
+    if !dpi_aware.as_bool() {
+        eprintln!("Couldn't respect display scaling!");
     }
 }
 
-/// Performs initialization needed to create common controls such as task dialogs.
-fn initialize_common_controls() {
-    let icc = INITCOMMONCONTROLSEX {
-        dwSize: std::mem::size_of::<INITCOMMONCONTROLSEX>() as u32,
-        dwICC: ICC_STANDARD_CLASSES,
-    };
-    let result = unsafe { InitCommonControlsEx(&icc) };
-    if !result.as_bool() {
-        panic!("Failed to initialize common controls!");
+/// Shows an informational message box.
+fn display_message_box(message: &[u16], title: PCWSTR) -> Result<(), Error> {
+    let lptext = PCWSTR(message.as_ptr());
+    let style = MB_OK | MB_ICONINFORMATION;
+
+    let result = unsafe { MessageBoxW(None, lptext, title, style) };
+
+    match result {
+        MESSAGEBOX_RESULT(0) => Err(Error::from_win32()),
+        _ => Ok(()),
     }
-}
-
-/// Displays a simple informational task dialog box, similar to a message box.
-fn display_task_dialog(
-    window_title: PCWSTR,
-    main_instruction: PCWSTR,
-    content: &[u16],
-) -> Result<(), Error> {
-    let pszcontent = PCWSTR(content.as_ptr());
-
-    unsafe {
-        TaskDialog(
-            None,
-            None,
-            window_title,
-            main_instruction,
-            pszcontent,
-            TDCBF_CLOSE_BUTTON,
-            TD_INFORMATION_ICON,
-            None,
-        )?;
-    };
-
-    Ok(())
 }
 
 fn main() -> Result<(), Error> {
-    initialize_com().expect("initializing COM should succeed");
-    initialize_common_controls();
+    attempt_dpi_awareness();
 
     let lines: Vec<String> = std::env::args()
         .enumerate()
         .map(|(i, arg)| format!("{i}: [{arg}]"))
         .collect();
 
-    let content: Vec<u16> = lines
+    let message: Vec<u16> = lines
         .join("\n")
         .encode_utf16()
         .chain(std::iter::once(0))
         .collect();
 
-    display_task_dialog(
-        w!("showargsw"),
-        w!("The following command-line arguments were passed."),
-        &content,
-    )
+    display_message_box(&message, w!("Command-line arguments"))
 }
