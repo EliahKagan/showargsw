7a1c147478fc518a978b606340cc33434b65665d
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-06 02:15:10 -0400

    Refactor parameter/variable names and spacing

--- src/main.rs
+++ src/main.rs
@@ -10,30 +10,27 @@ use windows::{
 fn main() -> Result<(), Error> {
     attempt_dpi_awareness();
 
-    let message = std::env::args()
+    let text = std::env::args()
         .enumerate()
         .map(|(i, arg)| format!("{i}: [{arg}]"))
         .collect::<Vec<_>>()
         .join("\n");
 
-    display_message_box(&message.into(), h!("Command-line arguments"))
+    display_message_box(&text.into(), h!("Command-line arguments"))
 }
 
 /// The text may still be too small, but this at least keeps it from being blurry.
 fn attempt_dpi_awareness() {
     let dpi_aware = unsafe { SetProcessDPIAware() };
-
     if !dpi_aware.as_bool() {
         eprintln!("Couldn't respect display scaling!");
     }
 }
 
 /// Shows an informational message box.
-fn display_message_box(message: &HSTRING, title: &HSTRING) -> Result<(), Error> {
+fn display_message_box(text: &HSTRING, caption: &HSTRING) -> Result<(), Error> {
     let style = MB_OK | MB_ICONINFORMATION;
-
-    let result = unsafe { MessageBoxW(None, message, title, style) };
-
+    let result = unsafe { MessageBoxW(None, text, caption, style) };
     match result {
         MESSAGEBOX_RESULT(0) => Err(Error::from_win32()),
         _ => Ok(()),
